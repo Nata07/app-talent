dfb9d5467c4498779841237138dc12cf8dc73ffa
Natanael <nata.mw@hotmail.com>
2023-09-04 23:03:48 -0300

    feat: added signin in app js

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'react-native';
-import {useFonts, Poppins_400Regular, Poppins_600SemiBold} from '@expo-google-fonts/poppins';
-import { NativeBaseProvider, Box, Text } from 'native-base';
+import {useFonts, Poppins_400Regular, Poppins_800ExtraBold} from '@expo-google-fonts/poppins';
+import { NativeBaseProvider } from 'native-base';
 import { Loading } from './src/Component/Loading';
 import { THEME } from './src/theme';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
 
   let [fontsLoaded] = useFonts({
-    Poppins_400Regular, Poppins_600SemiBold
+    Poppins_400Regular, Poppins_800ExtraBold
   });
 
   if (!fontsLoaded) {
@@ -16,11 +17,8 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <Box style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
         <StatusBar translucent barStyle="dark-content" />
-        {fontsLoaded ? <Text style={{fontFamily: 'Poppins_600SemiBold'}}>Hello World</Text> : <Loading /> }
-        
-      </Box>
+        {fontsLoaded ? <SignIn /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
