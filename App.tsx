1e4742c89dab1b30f2326e6d22d38d6a91c2eb5b
Natanael <nata.mw@hotmail.com>
2023-09-04 21:39:09 -0300

    feat: added theme and component spinner

--- App.tsx
+++ App.tsx
@@ -1,9 +1,26 @@
-import { Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import {useFonts, Poppins_400Regular, Poppins_600SemiBold} from '@expo-google-fonts/poppins';
+import { NativeBaseProvider, Box, Text } from 'native-base';
+import { Loading } from './src/Component/Loading';
+import { THEME } from './src/theme';
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Poppins_400Regular, Poppins_600SemiBold
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <Box style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+        <StatusBar translucent barStyle="dark-content" />
+        {fontsLoaded ? <Text style={{fontFamily: 'Poppins_600SemiBold'}}>Hello World</Text> : <Loading /> }
+        
+      </Box>
+    </NativeBaseProvider>
   );
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,36 @@
+import {extendTheme} from 'native-base'
+
+export const THEME = extendTheme(
+{
+    colors: {
+      green: {
+        500: '#3EB489',
+      },
+      gray: {
+        200: '#64748B',
+        300: '#828282',
+        400: '#3B3B3B',
+        500: '#1E293B',
+        900: '#000113',
+      },
+      white: '#FFFFFF',
+      red: {
+        500: '#F75A68'
+      }
+    },
+    fonts: {
+      heading: 'Poppins_400Regular',
+      body: 'Poppins_600SemiBold',
+    },
+    fontSizes: {
+      xs: 12,
+      sm: 14,
+      md: 16,
+      lg: 18,
+      xl: 20,
+    },
+    sizes: {
+      14: 56,
+      33: 148
+    }
+  })
