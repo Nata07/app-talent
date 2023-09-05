72c4d1c5eb93ba715fb7909991daa3547a91f333
Natanael <nata.mw@hotmail.com>
2023-09-04 23:42:34 -0300

    feat: create sign up page

---
+++ src/screens/SignUp/index.tsx
@@ -0,0 +1,30 @@
+import { Box, Center, HStack, Heading, Image, Text, VStack, ScrollView } from "native-base";
+import BackgroundImage from '../../assets/background.png';
+import LogoImage from '../../assets/Brand.svg';
+import { Input } from "../../Component/Input";
+import { Button } from "../../Component/Button";
+
+export function SignUp() {
+    return (
+        <ScrollView contentContainerStyle={{flexGrow: 1,}} showsVerticalScrollIndicator={false}>
+            <VStack flex={1} bg="white" pb={24}>
+                <Image w="full" source={BackgroundImage} alt="Background Image SignIn" resizeMode="cover" position="absolute" />
+                <Box mt={82} w="full" alignItems="center">
+                    <LogoImage />
+                </Box>
+                <Center mt={48}>
+                    <Heading fontSize={36} fontFamily="heading" letterSpacing={2} color="gray.500">Sign Up</Heading>
+                    <Box mt={10} w="full" px={8}>
+                        <Input label="Name" placeholder="Digite seu nome" />
+                        <Input label="Surname" placeholder="Digite seu email" />
+                        <Input label="Email" keyboardType="email-address" autoCapitalize="none" placeholder="Digite seu email" />
+                        <Input label="Password" secureTextEntry placeholder="Digite sua senha" />
+                        <Input label="Confirm Password" secureTextEntry placeholder="Cofirme sua senha" />
+
+                        <Button title="Log In" bg="gray.900" mb={24} />
+                    </Box>
+                </Center>
+            </VStack>
+        </ScrollView>  
+    )
+}
