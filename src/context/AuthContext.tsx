e1698a428e83187054ee31a8439e7a09d1a99e75
Natanael <nata.mw@hotmail.com>
2023-09-06 01:04:04 -0300

    feat: added context user initial

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,18 @@
+import { ReactNode, createContext } from "react";
+import { UserDTO } from "../dtos/UserDTO";
+
+export type AuthContextDataProps = {
+    // user: UserDTO;
+}
+
+type AuthContextProviderProps = {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+    <AuthContext.Provider value={{}}>
+        {children}
+    </AuthContext.Provider>
+}
