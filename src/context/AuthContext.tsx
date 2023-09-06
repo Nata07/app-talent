7a7c7115dabfb84a1909934e2d6d8348fb5dbe3b
Natanael <nata.mw@hotmail.com>
2023-09-06 20:34:53 -0300

    feat: added hook auth initial

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,8 +1,9 @@
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useState } from "react";
 import { UserDTO } from "../dtos/UserDTO";
 
 export type AuthContextDataProps = {
-    // user: UserDTO;
+    user: UserDTO;
+    signIn: (email: string, password: string) => void
 }
 
 type AuthContextProviderProps = {
@@ -12,7 +13,25 @@ type AuthContextProviderProps = {
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
-    <AuthContext.Provider value={{}}>
-        {children}
-    </AuthContext.Provider>
+    const [user, setUser] = useState<UserDTO>({} as UserDTO);
+
+    function signIn(email: string, password: string) {
+        try {
+            setUser({
+                id: '1',
+                name: 'Natanael',
+                surname: 'Silva',
+                email: email,
+                avatar: 'https://avatars.githubusercontent.com/u/13895506?v=4'
+            })
+        } catch (error) {
+            console.log(error)
+            throw new Error('Nao foi possivel fazer login')
+        }
+    }
+    return (
+        <AuthContext.Provider value={{user, signIn}}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
