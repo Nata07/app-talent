fbe0c4bdd052527ee762385447f836f2e44056fa
Natanael <nata.mw@hotmail.com>
2023-09-07 01:24:40 -0300

    feat: added icon tab bottom

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -5,6 +5,7 @@ import { Profile } from '../screens/Profile';
 import HomeIcon from '../assets/home.svg'
 import ProfileIcon from '../assets/profile.svg'
 import SettingsIcon from '../assets/setting.svg'
+import SAvedIcon from '../assets/heart.svg'
 import { Platform } from 'react-native';
 
 type AppRoutes = {
@@ -22,13 +23,48 @@ const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
     return (
-        <Navigator screenOptions={{
-            headerShown: false
-        }}>
-            <Screen name='home' component={Home} />
-            <Screen name='saved' component={Profile} />
-            <Screen name='settings' component={Profile} />
-            <Screen name='profile' component={Profile} />
+        <Navigator  
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: "#3EB489",
+            }}
+        >
+            <Screen 
+                name='home' 
+                component={Home} 
+                options={{  
+                    tabBarIcon: () => (
+                        <HomeIcon />
+                    )
+                }}
+            />
+            <Screen 
+                name='saved' 
+                component={Profile}
+                options={{
+                    tabBarIcon: () => (
+                        <SAvedIcon />
+                    )
+                }}
+            />
+            <Screen 
+                name='settings' 
+                component={Profile}
+                options={{
+                    tabBarIcon: () => (
+                        <SettingsIcon />
+                    )
+                }}
+            />
+            <Screen 
+                name='profile' 
+                component={Profile}
+                options={{
+                    tabBarIcon: () => (
+                        <ProfileIcon />
+                    )
+                }}
+            />
         </Navigator>
     )
 }
